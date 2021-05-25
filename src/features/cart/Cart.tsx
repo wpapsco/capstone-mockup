c39168e716c1e9257d885afd616d956ecae34b29
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-25 14:56:14 -0700

    Create React component for Cart

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,9 +1,32 @@
+import { dateTimeFormatter } from '@material-ui/data-grid'
+import { eventNames } from 'process'
+import { appSelector, useAppDispatch } from '../../app/hooks'
+import { selectContents, Ticket } from './cartSlice'
+
 const Cart = () => {
+    const contents = appSelector(selectContents)
+
     return (
         <div>
             <h1>Cart</h1>
+            {contents.tickets.map(ticket => 
+                <TicketCard
+                    key={ticket.eventId.toString() + ticket.participantId.toString()}
+                    {...ticket} />
+            )}
         </div>
     )
 }
+
+export const TicketCard = (data: Ticket) => {
+    return (
+        <article>
+            <h2>{data.eventName}</h2>
+            <h4>{data.participantName}</h4>
+            <p>Concessions: {data.concessions ? 'Yes' : 'No'}</p>
+            <p>{data.datetime.toDateString}</p>
+        </article>
+    )
+} 
 
 export default Cart
