b3730a229e208a8948c2817044ee4ca86f63d336
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-25 15:04:42 -0700

    Configure UI for adding tickets

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,24 +1,43 @@
-import { dateTimeFormatter } from '@material-ui/data-grid'
-import { eventNames } from 'process'
 import { appSelector, useAppDispatch } from '../../app/hooks'
-import { selectContents, Ticket } from './cartSlice'
+import { selectContents, Ticket, addTicket } from './cartSlice'
+
+import Button from '@material-ui/core/Button';
 
 const Cart = () => {
     const contents = appSelector(selectContents)
+
+    const dispatch = useAppDispatch()
+
+    const handleAddTicket = () => {
+        const ticketData: Ticket = {
+            eventName: 'Fundraiser',
+            eventId: 1,
+            participantId: 1,
+            participantName: 'Jane Doe',
+            concessions: true,
+            datetime: new Date(Date.now())
+        }
+
+        dispatch(addTicket(ticketData))
+    }
 
     return (
         <div>
             <h1>Cart</h1>
             {contents.tickets.map(ticket => 
-                <TicketCard
+                <TicketSummary
                     key={ticket.eventId.toString() + ticket.participantId.toString()}
                     {...ticket} />
             )}
+            <Button
+                variant='contained'
+                color='primary'
+                onClick={() => handleAddTicket()}>Add Ticket Test</Button>
         </div>
     )
 }
 
-export const TicketCard = (data: Ticket) => {
+export const TicketSummary = (data: Ticket) => {
     return (
         <article>
             <h2>{data.eventName}</h2>
