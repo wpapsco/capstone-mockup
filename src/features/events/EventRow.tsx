3bf18b940e60b94287472e01245eef03abfdab14
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-27 23:07:21 -0700

    Clean up event row

--- src/features/events/EventRow.tsx
+++ src/features/events/EventRow.tsx
@@ -1,15 +1,10 @@
-import Button from '@material-ui/core/Button';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Card from '@material-ui/core/Card';
 import {makeStyles} from '@material-ui/core/styles';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { EventDetails } from './eventsSlice';
 import Showtime from '../../components/Showtime';
-import { findByLabelText } from '@testing-library/dom';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 
 import { Link } from 'react-router-dom'
 
@@ -52,29 +47,3 @@ export default function EventRow(props: EventDetails) {
             </Card>
     )
 }
-
-// return (
-//     <Card className={classes.root}>
-//         <div className={classes.sideActions}>
-//             <AddShoppingCartIcon />
-//             <BookmarkBorderIcon />
-//         </div>
-
-//         <div className={classes.details}>
-//             <CardContent className={classes.content}>
-//                 <Typography variant="h5" component="h2">
-//                     {props.name}
-//                 </Typography>
-//                 <Showtime date={props.date}/>
-//                 <Typography variant="body2" color="textSecondary" component="p">
-//                     {props.shortDesc}
-//                 </Typography>
-//             </CardContent>
-//         </div>
-//         <CardMedia
-//             className={classes.media}
-//             image={props.imgUrl}
-//             title="Contemplative Reptile"
-//         />
-//     </Card>
-// );
