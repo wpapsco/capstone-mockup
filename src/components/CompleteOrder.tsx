3dda2aae90c5b0315db1ac11a987fb03f68bffdf
Geoff Corvera <geoffcorvera@gmail.com>
2021-06-04 15:58:29 -0700

    Breat out UI elements into separate, composed components

--- src/components/CompleteOrder.tsx
+++ src/components/CompleteOrder.tsx
@@ -1,106 +1,61 @@
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-import { appSelector } from '../app/hooks'
-import { selectCartItems } from '../features/cart/cartSlice'
-import { useEffect, useState } from 'react';
+import YourOrder from '../features/cart/YourOrder'
 
-const toDollar = (x: number) => `$${(Math.round(x * 100) / 100).toFixed(2)}`
 
 export default function CompleteOrder() {
-    const cartItems = appSelector(selectCartItems)
-    const [subtotal, setSubtotal] = useState(0)
 
-    useEffect(() => {
-        setSubtotal(
-            cartItems.reduce((tot, curItem) => tot += curItem.quantity * curItem.unitPrice, 0)
-        )
-    }, [cartItems])
-
-    return <div style={{display: "flex", height: "100vh", width: "100%"}}>
-        <Paper style={{minWidth: "20%", flexGrow: 2, height: "100%", margin: "10px", paddingLeft: "2%", paddingRight: "2%", paddingTop: "50px"}} variant="outlined">
-            <Typography variant="h4">Your order</Typography>
-            {cartItems.map(data => <CartItem {...data} />)}
-
-            <Button color="primary" variant="contained" style={{width: "100%"}}>Add more items</Button>
-            <Divider style={{marginBottom: "30px", marginTop: "30px"}}/>
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant="body1">Subtotal</Typography>
-                <Typography variant="body2" color="textSecondary">{toDollar(subtotal)}</Typography>
-            </div>
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant="body2">Discount</Typography>
-                <Typography variant="body2" color="textSecondary">-$X.XX</Typography>
-            </div>
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant="body2">Donation</Typography>
-                <Typography variant="body2" color="textSecondary">$X.XX</Typography>
-            </div>
-            <Divider style={{marginBottom: "30px", marginTop: "30px"}}/>
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Typography variant="body1">Total</Typography>
-                <Typography variant="body1" color="textSecondary">$X.XX</Typography>
-            </div>
-        </Paper>
-        <Paper style={{flexGrow: 8, height: "100%", margin: "10px", paddingLeft: "5%", paddingTop: "50px", paddingRight: "10%"}} variant="outlined">
-            <Typography variant="h3">Complete Order</Typography>
-            <Typography variant="h4">Contact</Typography>
-            <form>
-                {true && <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <TextField id="first-name" label="First Name" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="last-name" label="Last Name" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField id="street-address" label="Street Address" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="postal-code" label="Postal Code" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="country" label="Country" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="phone" label="Phone" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="email" label="Email" variant="outlined" fullWidth/>
-                        <FormControlLabel control={<Checkbox />} label="Email opt-in"/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="visit-source" label="How did you hear about us?" variant="outlined" fullWidth/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField id="comments" label="Comments" variant="outlined" fullWidth multiline/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Button variant="contained" style={{width: "100%"}}>Back</Button>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Button variant="contained" color="primary" style={{width: "100%"}}>Next</Button>
-                    </Grid>
-                </Grid>}
-            </form>
-        </Paper>
-    </div>
-}
-
-export type CartItemProps = { name: string, description: string, quantity: number, unitPrice: number }
-function CartItem(props: CartItemProps) {
-    return <div style={{paddingBottom: "10px"}}>
-        <Typography variant="body1" color="textPrimary">{props.name}</Typography>
-        <Typography variant="body2" color="textSecondary">{props.description}</Typography>
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-            <Typography variant="body2" color="textSecondary">Quantity: {props.quantity}</Typography>
-            <Typography variant="body2" color="textSecondary">${props.unitPrice}</Typography>
+    return (
+        <div style={{display: "flex", height: "100vh", width: "100%"}}>
+            <YourOrder />
+            <Paper style={{flexGrow: 8, height: "100%", margin: "10px", paddingLeft: "5%", paddingTop: "50px", paddingRight: "10%"}} variant="outlined">
+                <Typography variant="h3">Complete Order</Typography>
+                <Typography variant="h4">Contact</Typography>
+                <form>
+                    {true && <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <TextField id="first-name" label="First Name" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="last-name" label="Last Name" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id="street-address" label="Street Address" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="postal-code" label="Postal Code" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="country" label="Country" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="phone" label="Phone" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="email" label="Email" variant="outlined" fullWidth/>
+                            <FormControlLabel control={<Checkbox />} label="Email opt-in"/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField id="visit-source" label="How did you hear about us?" variant="outlined" fullWidth/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField id="comments" label="Comments" variant="outlined" fullWidth multiline/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button variant="contained" style={{width: "100%"}}>Back</Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button variant="contained" color="primary" style={{width: "100%"}}>Next</Button>
+                        </Grid>
+                    </Grid>}
+                </form>
+            </Paper>
         </div>
-    </div>
+    )
 }
---
+++ src/features/cart/YourOrder.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import { appSelector } from '../../app/hooks'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button'
+import { selectCartItems } from './cartSlice'
+import CartItem from './CartItem'
+
+const toDollar = (x: number) => `$${(Math.round(x * 100) / 100).toFixed(2)}`
+
+const YourOrder = () => {
+    const cartItems = appSelector(selectCartItems)
+    const [subtotal, setSubtotal] = useState(0)
+
+    useEffect(() => {
+        setSubtotal(
+            cartItems.reduce((tot, curItem) => tot += curItem.quantity * curItem.unitPrice, 0)
+        )
+    }, [cartItems])
+
+    const contents = (cartItems.length > 0) ? cartItems.map(data => <CartItem {...data} />)
+        : <p>Cart empty</p>
+        
+    return (
+        <Paper style={{minWidth: "20%", flexGrow: 2, height: "100%", margin: "10px", paddingLeft: "2%", paddingRight: "2%", paddingTop: "50px"}} variant="outlined">
+            <Typography variant="h4">Your order</Typography>
+            {contents}
+
+            <Button color="primary" variant="contained" style={{width: "100%"}}>Add more items</Button>
+            <Divider style={{marginBottom: "30px", marginTop: "30px"}}/>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Typography variant="body1">Subtotal</Typography>
+                <Typography variant="body2" color="textSecondary">{toDollar(subtotal)}</Typography>
+            </div>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Typography variant="body2">Discount</Typography>
+                <Typography variant="body2" color="textSecondary">-$X.XX</Typography>
+            </div>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Typography variant="body2">Donation</Typography>
+                <Typography variant="body2" color="textSecondary">$X.XX</Typography>
+            </div>
+            <Divider style={{marginBottom: "30px", marginTop: "30px"}}/>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Typography variant="body1">Total</Typography>
+                <Typography variant="body1" color="textSecondary">$X.XX</Typography>
+            </div>
+        </Paper>
+    )
+}
+
+export default YourOrder
