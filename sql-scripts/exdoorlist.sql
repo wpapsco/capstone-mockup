d43f14ee9d28450468bcc15168798efc9261cc59
Francis Phan <phan5@pdx.edu>
2021-06-20 18:43:55 -0700

    Add SQL code to create exdoorlist table for PoC

---
+++ sql-scripts/exdoorlist.sql
@@ -0,0 +1,10 @@
+CREATE TABLE exdoorlist
+(
+    id integer NOT NULL,
+    name text,
+    vip boolean,
+    donor boolean,
+    accomodations boolean,
+    num_tickets integer,
+    arrived boolean
+);
