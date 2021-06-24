9bc28121faedfd14c9098d52de6177315fdba164
JECoyle <Zilarrezko76@gmail.com>
2021-06-24 15:47:05 -0700

    Checkpoint. Added functionality for newsletter to update and insert database information based on json data. Hooked up submit button to make necessary queries. There still remains for getting form information, checking if the email is already with us. From there it should just be plug and play and an easy email signup!
    Thank you, Francis

--- server/server.ts
+++ server/server.ts
@@ -36,5 +36,51 @@ app.get('/api/doorlist', async (req, res) => {
     }
 });
 
+app.post('/api/newsletter/count', async (req, res) => {
+    try
+    {
+        const emails = await pool.query("SELECT COUNT(*) FROM customers WHERE email = $1", [req.body.email]);
+        res.json(emails.rows);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+});
+
+app.post('/api/newsletter/update', async (req, res) => {
+    try
+    {
+        var body = req.body;
+        var values = [body.news_opt, body.volunteer_opt, body.email];
+        const rows = await pool.query("UPDATE public.customers\
+            SET newsletter=$1, \"volunteer list\"=$2\
+            WHERE email = $3;", values);
+        res.json(rows.rows);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+});
+
+app.post('/api/newsletter/insert', async (req, res) => {
+    try
+    {
+        var body = req.body;
+        var values = [body.custname, body.email,
+                      body.phone, body.custaddress, body.news_opt,
+                      body.donorbadge, body.seatingaccom, body.vip, body.volunteer_opt];
+        const emails = await pool.query("INSERT INTO public.customers(\
+            custname, email, phone, custaddress, newsletter, donorbadge, seatingaccom, vip, \"volunteer list\")\
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);", values);
+        res.json(emails.rows);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+});
+
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/features/newsletter/NewsletterPage.tsx
+++ src/features/newsletter/NewsletterPage.tsx
@@ -17,6 +17,93 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export default function NewsletterPage()
 {
+   const submit = async () =>
+   {
+      // Check if email is valid
+      var email = 'aosihdfbgoasiduhjgfiuhasdvfg@tutanota.com';
+      var news_opt = false;
+      var volunteer_opt = true;
+
+      // Check if email is already with us
+      
+
+      var email_exists = false;
+      try
+      {
+         const data = {email: email};
+         const response = await fetch("http://localhost:5000/api/newsletter/count",
+            {
+               method: 'POST',
+               headers: {
+                  'Content-Type': 'application/json',
+               },
+               body: JSON.stringify(data),
+            });
+         const jsonData = await response.json();
+         email_exists = +jsonData[0].count > 0;
+      }
+      catch(error)
+      {
+         console.error(error.message);
+      }
+
+      if(email_exists)
+      {
+         // Need to get checkbox things
+         try
+         {
+            const data = {news_opt: news_opt, volunteer_opt: volunteer_opt, email: email};
+            const response = await fetch("http://localhost:5000/api/newsletter/update",
+               {
+                  method: 'POST',
+                  headers: {
+                     'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(data),
+               });
+            const jsonData = await response.json();
+            console.log(jsonData);
+         }
+         catch(error)
+         {
+            console.error(error.message);
+         }
+      }
+      else
+      {
+         const data =
+         {
+            custname: "Bill Data",
+            email: "junk data@nice.com",
+            phone: "521-536-9278",
+            custaddress: "120 NorthPole Cd.",
+            news_opt: news_opt,
+            donorbadge: false,
+            seatingaccom: false,
+            vip: false,
+            volunteer_opt: volunteer_opt
+         };
+
+         try
+         {
+            const response = await fetch("http://localhost:5000/api/newsletter/insert",
+               {
+                  method: 'POST',
+                  headers: {
+                     'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(data),
+               });
+            const jsonData = await response.json();
+            console.log(jsonData);
+         }
+         catch(error)
+         {
+            console.error(error.message);
+         }
+      }
+   };
+
    return (
       <div style={{display: "flex", height: "100vh", width: "100%"}}>
          <Paper style={{flexGrow: 8, height: "100%", margin: "10px", paddingLeft: "5%", paddingTop: "50px", paddingRight: "10%"}} variant="outlined">
@@ -32,7 +119,7 @@ export default function NewsletterPage()
                <TextField id="state" label="State" variant="outlined" fullWidth/>
                <TextField id="zip" label="Zip Code" variant="outlined" fullWidth/>
                <TextField id="phone" label="Phone" variant="outlined" fullWidth/>
-               <Button variant="contained" color="primary" style={{width: "50%"}}>Sign-up</Button>
+               <Button variant="contained" color="primary" onClick={() => {submit()}} style={{width: "50%"}}>Sign-up</Button>
             </form>
          </Paper>
       </div>
