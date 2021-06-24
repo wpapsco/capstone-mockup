34b73f79383ff421abf00bf95361cf987d48f0d1
Wpapsco <wpapsco@gmail.com>
2021-06-24 14:25:51 -0700

    actually send form fields to server in CompleteOrder

--- server/server.ts
+++ server/server.ts
@@ -29,6 +29,7 @@ app.use(express.urlencoded({extended: true}));
 // };
 // app.post('/api/messages', postMessages);
 
+//TODO: find a way to hide api calls like this behind some kind of auth
 app.get('/api/doorlist', async (req, res) => {
     try{
         const doorlist = await pool.query("SELECT * FROM exdoorlist");
@@ -39,15 +40,20 @@ app.get('/api/doorlist', async (req, res) => {
     }
 });
 
+// TODO: when we add confirmation emails we can do it like this:
+// https://stripe.com/docs/payments/checkout/custom-success-page
 app.post('/api/checkout', async (req, res) => {
     // TODO: NOT DO IT THIS WAY!!!
     // right now it gets the price info from the request made by the client.
     // THIS IS WRONG it needs to look up the price in the database given
     // the id of the show/event/whatever. PRICES CANNOT COME FROM CLIENTS!!
-    const data: CartItem<TicketData>[] = req.body;
+    const data: CartItem<TicketData>[] = req.body.cartItems;
+    // TODO: submit form data to DB
+    const formData = req.body.formData;
+    console.log(formData);
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        //this is the offending area
+        //this is the offending area all this stuff needs to be replaced by info from DB based on play ID or something
         line_items: data.map(item => ({
             price_data: {
                 currency: "usd",
@@ -55,11 +61,11 @@ app.post('/api/checkout', async (req, res) => {
                     name: item.name,
                     description: item.description
                 },
+                // the price here needs to be fetched from the DB instead
                 unit_amount: item.unitPrice * 100
             },
             quantity: item.quantity
         })),
-
         mode: "payment",
         success_url: "http://localhost:3000/success",
         cancel_url: "http://localhost:3000",
--- src/components/CompleteOrder.tsx
+++ src/components/CompleteOrder.tsx
@@ -1,20 +1,22 @@
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+// import TextField from '@material-ui/core/TextField';
+import { TextField, Checkboxes } from 'mui-rff';
+import Grid, { GridProps } from '@material-ui/core/Grid';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import YourOrder from '../features/cart/YourOrder'
 import { selectCartItems } from '../features/cart/cartSlice'
 import { appSelector } from '../app/hooks'
 import { loadStripe } from '@stripe/stripe-js';
+import { Field, Form } from 'react-final-form'
 
 const stripePromise = loadStripe("pk_test_51J5bpwGEweatMRnmGFUKgE6Q3wn7GmOJDAJ3Zag8DIhZjh324DdDUCFiEOLa0HQZFonkf2pc6lAOpPuheQs9N8AC00zNa4xALV")
 
 export default function CompleteOrder() {
     const cartItems = appSelector(selectCartItems)
-    const doCheckout = async () => {
+    const doCheckout = async (formData: {}) => {
         const stripe = await stripePromise;
         if (!stripe) return;
 
@@ -23,7 +25,7 @@ export default function CompleteOrder() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(cartItems)
+            body: JSON.stringify({cartItems, formData})
         })
 
         const session = await response.json()
@@ -43,44 +45,53 @@ export default function CompleteOrder() {
             <Paper style={{flexGrow: 8, height: "100%", margin: "10px", paddingLeft: "5%", paddingTop: "50px", paddingRight: "10%"}} variant="outlined">
                 <Typography variant="h3">Complete Order</Typography>
                 <Typography variant="h4">Contact</Typography>
-                <form>
-                    {true && <Grid container spacing={3}>
-                        <Grid item xs={6}>
-                            <TextField id="first-name" label="First Name" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="last-name" label="Last Name" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField id="street-address" label="Street Address" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="postal-code" label="Postal Code" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="country" label="Country" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="phone" label="Phone" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="email" label="Email" variant="outlined" fullWidth/>
-                            <FormControlLabel control={<Checkbox />} label="Email opt-in"/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <TextField id="visit-source" label="How did you hear about us?" variant="outlined" fullWidth/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField id="comments" label="Comments" variant="outlined" fullWidth multiline/>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Button variant="contained" style={{width: "100%"}}>Back</Button>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Button onClick={doCheckout} variant="contained" color="primary" style={{width: "100%"}}>Next</Button>
-                        </Grid>
-                    </Grid>}
-                </form>
+                <Form 
+                    onSubmit={doCheckout}
+                    initialValues={{"opt-in": false}}
+                    render={({ handleSubmit, values }) => (
+                        <form onSubmit={handleSubmit} noValidate>
+                            <Grid container spacing={3}>
+                                <Grid item xs={6}>
+                                    <TextField name="first-name" label="First Name" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="last-name" label="Last Name" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField name="street-address" label="Street Address" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="postal-code" label="Postal Code" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="country" label="Country" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="phone" label="Phone" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="email" label="Email" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField name="visit-source" label="How did you hear about us?" variant="outlined" fullWidth/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Checkboxes name="opt-in" data={{ label: 'Email opt-in', value: true }} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField name="comments" label="Comments" variant="outlined" fullWidth multiline/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Button variant="contained" style={{width: "100%"}}>Back</Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Button type="submit" variant="contained" color="primary" style={{width: "100%"}}>Next</Button>
+                                </Grid>
+                            </Grid>
+                            {JSON.stringify(values)}
+                        </form>
+                    )}
+                />
             </Paper>
         </div>
     )
