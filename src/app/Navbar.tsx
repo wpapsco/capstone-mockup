ce2fbe63230009247934b009b8b0d3e06051b927
Geoff Corvera <geoffcorvera@gmail.com>
2021-07-01 17:47:21 -0700

    Add shopping cart icon to navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,18 +1,44 @@
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles'
+import { theme } from '../theme'
 
 import { NavLink } from 'react-router-dom'
 
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        navbar: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingRight: '30px',
+        },
+        // link: {
+        //     '&::visited': {
+        //         color: 'grey'
+        //     }
+        // },
+    })
+)
+
+
+// TODO: Create shopping cart icon component (with number of items in cart)
+// TODO: Remove visited link color from shopping cart icon
 export default function Navbar()  {
+    const classes = useStyles(theme)
+
     return (
-        <Paper square>
+        <Paper square className={classes.navbar}>
             <Tabs>
                 <Tab label="Doorlist" component={NavLink} to="/" />
                 <Tab label="Events" component={NavLink} to="/events" />
-                <Tab label="Complete Order" component={NavLink} to="/completeorder"/>
-                <Tab label="Create Events" component={NavLink} to="/CreateEvents"/>
+                <Tab label="Complete Order" component={NavLink} to="/completeorder" />
+                <Tab label="Create Events" component={NavLink} to="/CreateEvents" />
             </Tabs>
+            <NavLink to="/cart"><ShoppingCartIcon /></NavLink>
         </Paper>
-)
+    )
 }
+
