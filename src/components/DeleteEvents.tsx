e0a869b3bf7fe3b22d95e6a85cb07c407bb31bc7
Francis Phan <phan5@pdx.edu>
2021-07-08 14:10:26 -0700

    Added button functionality to remove events. Need to work on refreshing table afterwards.

--- src/components/DeleteEvents.tsx
+++ src/components/DeleteEvents.tsx
@@ -1,23 +1,46 @@
-import TextField from '@material-ui/core/TextField';
 import { DataGrid } from '@material-ui/data-grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import React, { useState, useEffect } from "react";
 
-// export default function DeleteEvents(props: {}) {
-//     return (
-//         <div>
-//             <h1>Enter Name of Event to Delete</h1>
-//             <TextField id="EventName" label="Event Name" variant="outlined" fullWidth/>
-//             <button type="button">Delete</button>
-//         </div>
-//     );
-// }
-
 export default function DeleteEvents() {
-    function deleteEvent() {
-        return;
+    async function deleteEvent() {
+        const data = {
+            id: selectedRow.id,
+            playname: selectedRow.playname,
+            playdescription: selectedRow.playdescription,
+            eventdate: selectedRow.eventdate,
+            starttime: selectedRow.starttime
+        }
+
+        const response = await fetch("http://localhost:5000/api/delete-event",
+        {
+           method: 'POST',
+           headers:
+           {
+              'Content-Type': 'application/json',
+           },
+           body: JSON.stringify(data),
+        });
+        return response.json();
     }
+
+    //Add row data to hook
+    function onSelection(row:any) {
+        const convert =  row.rows[0]
+        setSelectedRow(convert);
+    }
+
+    //Generate hook structure
+    const[selectedRow, setSelectedRow] = useState({
+        id: "",
+        playname: "",
+        playdescription: "",
+        eventdate: "",
+        starttime: ""
+    });
+
+    // Create columns that appears in data
     const columns = [
         { field: "id", headerName: "Showtime ID", width: 100},
         { field: "playname", headerName: "Play", width: 150},
@@ -28,6 +51,7 @@ export default function DeleteEvents() {
             <Button variant="contained" color="secondary" onClick={() => deleteEvent()}>Delete</Button>
         )}
     ]
+
     const [eventList, setEventList] = useState([]);
     const getEvents = async () => {
         try {
@@ -43,7 +67,7 @@ export default function DeleteEvents() {
     return (
         <div>
             <Typography variant="h2">Delete Events</Typography>
-            <DataGrid autoHeight rows={eventList} columns={columns} pageSize={10}/>
+            <DataGrid autoHeight rows={eventList} columns={columns} onSelectionChange={onSelection} pageSize={10} />
         </div>
     );
 }
