e318e5b3dcc2163fdbc2c7b38718738d082141cd
Geoff Corvera <geoffcorvera@gmail.com>
2021-07-10 09:14:59 -0700

    add captialize function to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,6 @@
 export const urlFriendly = (s: string) => s.replace(/ /g, '_')
+
+export const capitalize = (s: string) =>
+    s.split(' ')
+        .map((word: string) => word.slice(0,1).toUpperCase() + word.slice(1))
+        .join(' ')
