8d12dc46b72a87c4add8e1a92203e2efddbed5b8
Geoff Corvera <geoffcorvera@gmail.com>
2021-07-10 00:59:16 -0700

    clean up old events stuff from AllEventsPage

--- src/features/events/AllEventsPage.tsx
+++ src/features/events/AllEventsPage.tsx
@@ -3,12 +3,14 @@ import { appSelector, useAppDispatch } from '../../app/hooks'
 import { useEffect } from 'react'
 
 import { Typography } from '@material-ui/core'
-import EventRow from './EventRow'
+// import EventRow from './EventRow'
 
 export default function AllEventsPage() {
     const dispatch = useAppDispatch()
     const allEvents = appSelector(selectAllEvents)
     const loadStatus = appSelector(state => state.events.status)
+
+    // const eventSummaries = Object.key
 
     useEffect(() => {
         if (loadStatus === 'idle') {
@@ -16,16 +18,14 @@ export default function AllEventsPage() {
         }
     }, [dispatch])
 
-
-    if (allEvents === undefined || allEvents.length === 0) {
+    if (allEvents === {}) {
         return <div>No events to show. Please check again later.</div>
     }
 
-    // TODO: Make event data in redux is compatible with EventRow component
+    // TODO: Add EventRow
     return (
         <section>
             <Typography variant='h4' component='h1'>Upcoming Events</Typography>
-            {allEvents.map(details => <EventRow {...details} />)}
         </section>
     )
 }
