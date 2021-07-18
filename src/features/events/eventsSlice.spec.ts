6b0809c694eba5e2125d45438cec474b4c1d5b15
Geoff Corvera <geoffcorvera@gmail.com>
2021-07-18 10:20:31 -0700

    refactor to simplify aggregating showings by play & update tests

--- src/features/events/eventsSlice.spec.ts
+++ src/features/events/eventsSlice.spec.ts
@@ -1,4 +1,4 @@
-import { groupPlays, Event, selectAllEvents } from "./eventsSlice";
+import { Event, selectAllEvents, aggregateShowings } from "./eventsSlice";
 import { urlFriendly, titleCase, militaryToCivilian, dayMonthDate } from '../../utils'
 
 const testEventData: Event[] = [
@@ -71,10 +71,48 @@ const Plays = {
     ]
 }
 
+const PlaysV2 = {
+    foo_Bar_baz: {
+        playname: 'foo Bar baz',
+        playdescription: 'desc1',
+        image_url: 'https://i.guim.co.uk/img/media/b5df93588386c0565177648cf41f3aff72c63400/0_217_5657_3395/master/5657.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=a917ce8d52959d36bb08ad29184e2701',
+        showings: [
+            {
+                id: 1,
+                eventdate: "2021-01-07T08:00:00.000Z",
+                starttime: "19:00:00",
+                totalseats: 1,
+                availableseats: 1,
+            },
+            {
+                id: 2,
+                eventdate: "2021-01-08T08:00:00.000Z",
+                starttime: "22:00:00",
+                totalseats: 3,
+                availableseats: 3,
+            },
+        ]
+    },
+    test_2: {
+        playname: 'test 2',
+        playdescription: 'desc1',
+        image_url: 'https://i.guim.co.uk/img/media/b5df93588386c0565177648cf41f3aff72c63400/0_217_5657_3395/master/5657.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=a917ce8d52959d36bb08ad29184e2701',
+        showings: [
+            {
+                id: 3,
+                eventdate: "2021-01-08T08:00:00.000Z",
+                starttime: "22:00:00",
+                totalseats: 3,
+                availableseats: 3,
+            },
+        ]
+    }
+}
+
 describe('Event slice utils', () => {
-    it('groupPlays', () => {
-        const groupedByPlays = groupPlays(testEventData)
-        expect(groupedByPlays).toEqual(Plays)
+
+    it('aggregateShowings', () => {
+        expect(aggregateShowings(testEventData)).toEqual(PlaysV2)
     })
 
     it('urlFriendly', () => {
@@ -91,14 +129,14 @@ describe('Event slice utils', () => {
 
     it('convert to dayMonthDate', () => {
         const d_str = '2021-01-07T08:00:00.000Z'
-        expect(dayMonthDate(d_str)).toEqual('Thu Jan 7')
+        expect(dayMonthDate(d_str)).toEqual('Thu, Jan 7')
     })
 })
 
 describe('Event slice selectors', () => {
     const InitState = {
         events: {
-            data: Plays,
+            data: PlaysV2,
             status: 'idle' as 'idle',
         },
         shop: { cart: [], donation: 0, },
