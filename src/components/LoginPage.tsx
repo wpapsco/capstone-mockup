fbf63ace9d95ce09bc4447e2878eb04bfb70cebf
Wpapsco <wpapsco@gmail.com>
2021-07-25 21:03:08 -0700

    pretty login page now

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,4 +1,4 @@
-import {Button, TextField} from "@material-ui/core";
+import {Button, TextField, Grid, Typography, Paper} from "@material-ui/core";
 import {useState} from "react";
 import {Redirect, useParams} from "react-router";
 import {useAppDispatch} from "../app/hooks";
@@ -30,17 +30,30 @@ export default function LoginPage() {
         }
     }
 
-    return <div>
-        <TextField
-            label="username"
-            variant="outlined"
-            onChange={e => setUsername(e.target.value)} />
-        <TextField
-            label="password"
-            variant="outlined"
-            type="password"
-            onChange={e => setPassword(e.target.value)} />
-        <Button variant="contained" color="primary" onClick={onSubmit}>Log in</Button>
-        {loggedIn && <Redirect to={"/" + (redirect ? redirect : "")}/>}
-    </div>
+    return <form onSubmit={e => e.preventDefault()}>
+        <Paper variant="outlined" style={{width: "50%", margin:"auto", padding: "15px"}}>
+            <Typography gutterBottom variant="h3" style={{textAlign: "center"}}>Please Log In</Typography>
+            <Grid container spacing={3} alignItems="flex-end">
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        label="username"
+                        variant="outlined"
+                        onChange={e => setUsername(e.target.value)} />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        label="password"
+                        variant="outlined"
+                        type="password"
+                        onChange={e => setPassword(e.target.value)} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" fullWidth color="primary" type="submit" onClick={onSubmit}>Log in</Button>
+                </Grid>
+            {loggedIn && <Redirect to={"/" + (redirect ? redirect : "")}/>}
+            </Grid>
+        </Paper>
+    </form>
 }
