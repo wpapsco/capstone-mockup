56785089c5e82882c6d3a248937129991c12e7a7
Geoff Corvera <geoffcorvera@gmail.com>
2021-08-20 23:05:14 -0700

    event creation: fetch ticket types & update styles

--- src/features/admin/eventCreation/v2/CreateEventPage.tsx
+++ src/features/admin/eventCreation/v2/CreateEventPage.tsx
@@ -1,15 +1,29 @@
+import { useEffect, useState } from 'react'
 import EventForm from './EventForm'
+import { Typography } from '@material-ui/core'
 
 
 export default function CreateEventPage() {
+    const [ticketTypes, setTicketTypes] = useState([])
+
+    const fetchTicketTypes = async () => {
+        const res = await fetch('/api/tickets/type')
+        const tTypes = await res.json()
+        setTicketTypes(tTypes)
+    }
+    
+    useEffect(() => {
+        fetchTicketTypes()
+    }, [])
+    
     const onSave = (formData: any) => {
         console.log('CreateEventPage', formData)
     }
 
     return (
         <div>
-            <h1>Create New Event</h1>
-            <EventForm onSave={onSave} />
+            <Typography variant='h3' component='h1'>Create New Event</Typography>
+            <EventForm onSave={onSave} ticketTypes={ticketTypes} />
         </div>
     )
 }
--- src/features/admin/eventCreation/v2/EventForm.tsx
+++ src/features/admin/eventCreation/v2/EventForm.tsx
@@ -3,12 +3,13 @@ import { TextField, KeyboardDateTimePicker, Select, Switches } from 'mui-rff'
 import arrayMutators from 'final-form-arrays'
 import { FieldArray } from 'react-final-form-arrays'
 import DateFnsUtils from "@date-io/date-fns";
-import { Button, MenuItem, Typography } from '@material-ui/core'
+import { Button, makeStyles, MenuItem, Theme, Typography } from '@material-ui/core'
 import { ValidationErrors } from 'final-form';
 
 
 // TODO: Get ticket types from DB
 interface TicketType {
+    id: number,
     name: string,
     price: number,
     concessions: number,
@@ -28,8 +29,10 @@ function validate(formData: any): ValidationErrors {
 
 interface EventFormProps {
     onSave: (formData: any) => void
+    ticketTypes: TicketType[]
 }
-export default function EventForm({onSave}: EventFormProps) {
+export default function EventForm({onSave, ticketTypes}: EventFormProps) {
+    const classes = useStyles()
 
     return (
         <Form
@@ -42,40 +45,49 @@ export default function EventForm({onSave}: EventFormProps) {
                 pristine,
                 submitting,
             }) => (
-                <form onSubmit={handleSubmit}>
-                    <TextField name='event-title' label='Event Title' required={true} />
-                    <TextField name='description' label='Description' />
-                    <TextField name='imageUrl' label='Image URL' />
+                <form className={classes.root} onSubmit={handleSubmit}>
+                    <TextField className={classes.field} name='playname' label='Event Title' required={true} />
+                    <TextField className={classes.field} name='playdescription' label='Description' />
+                    <TextField className={classes.field} name='image_url' label='Image URL' />
 
-                    <Typography variant='h3' component='h2'>Showings</Typography>
+                    <Typography variant='h4' component='h2' className={classes.heading}>Showings</Typography>
 
-                    <div>
-                        <Button variant='outlined' type='button' onClick={() => push('showings', undefined)}>
-                            Add Showing
-                        </Button>
+                    <div className={classes.buttonGroup}>
                         <Button variant='outlined' type='button' onClick={() => pop('showings')}>
                             Remove Showing
+                        </Button>
+                        <Button color='secondary' variant='outlined' type='button' onClick={() => push('showings', undefined)}>
+                            Add Showing
                         </Button>
                     </div>
                     
                     <FieldArray name='showings'>
                         {({ fields }) =>
                             fields.map((name, i) => (
-                                <div key={name}>
+                                <div key={name} className={classes.showing}>
                                     <label>Show # {i + 1}</label>
-                                    <KeyboardDateTimePicker
-                                        name={`${name}.DateTime`}
-                                        label='Event Date & Time'
-                                        required
-                                        dateFunsUtils={DateFnsUtils}
-                                    />
-                                    <TextField name={`${name}.totalseats`} label='Seating Capacity' type='number' required />
-                                    <Select name={`${name}.ticketType`} label='Select Ticket Type' required>
-                                        <MenuItem value='ticketType0'>General Admission - $15</MenuItem>
-                                        <MenuItem value='ticketType1'>General Admission - $20</MenuItem>
-                                    </Select>
-                                    <span onClick={() => fields.remove(i)} style={{cursor: 'pointer'}}>
-                                        X
+                                    <div className={classes.fieldGroup}>
+                                        <KeyboardDateTimePicker
+                                            name={`${name}.DateTime`}
+                                            label='Event Date & Time'
+                                            required
+                                            dateFunsUtils={DateFnsUtils}
+                                        />
+                                        <TextField name={`${name}.totalseats`} label='Seating Capacity' type='number' required />
+                                        <Select
+                                            name={`${name}.ticketType`}
+                                            label='Select Ticket Type'
+                                            required
+                                        >
+                                            {ticketTypes.map(t =>
+                                                <MenuItem key={t.id} value={t.id}>
+                                                    {`${t.name}: ${t.price} (+ ${t.concessions} concessions)`}
+                                                </MenuItem>
+                                            )}
+                                        </Select>
+                                    </div>
+                                    <span onClick={() => fields.remove(i)}>
+                                        Delete
                                     </span>
                                 </div>
                             ))
@@ -88,7 +100,7 @@ export default function EventForm({onSave}: EventFormProps) {
                         data={{label: '', value: true}}
                     />
 
-                    <Button variant='contained' color='primary' type='submit' disabled={submitting || pristine}>
+                    <Button className={classes.submitBtn} variant='contained' color='primary' type='submit' disabled={submitting || pristine}>
                         Save New Event
                     </Button>
                 </form>
@@ -96,3 +108,47 @@ export default function EventForm({onSave}: EventFormProps) {
         />
     )
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: '800px',
+    },
+    field: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(1),
+    },
+    buttonGroup: {
+        width: '50%',
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'flex',
+        justifyContent: 'space-around',
+    },
+    showing: {
+        margin: `${theme.spacing(2)}px 0`,
+        '& span': {
+            marginLeft: 'auto',
+            cursor: 'pointer'
+        }
+    },
+    fieldGroup: {
+        margin: `${theme.spacing(2)}px 0`,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        '& > *': {
+            margin: `0 ${theme.spacing(1)}px`
+        }
+    },
+    heading: {
+        marginTop: theme.spacing(5)
+    },
+    submitBtn: {
+        marginLeft: 'auto',
+    }
+}))
