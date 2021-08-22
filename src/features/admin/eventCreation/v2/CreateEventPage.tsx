16282e2a02cc47699abbf81207eda6d094d47dd1
Geoff Corvera <geoffcorvera@gmail.com>
2021-08-21 20:16:13 -0700

    Update Redux state when new event created & show onscreen feedback

--- src/features/admin/eventCreation/v2/CreateEventPage.tsx
+++ src/features/admin/eventCreation/v2/CreateEventPage.tsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
+import { useAppDispatch } from '../../../../app/hooks'
+import { fetchTicketingData } from '../../../ticketing/ticketingSlice'
+import { fetchEventData } from '../../../events/eventsSlice'
+import { openSnackbar } from '../../../snackbarSlice'
+
 import EventForm, { NewEventData } from './EventForm'
 import { Typography } from '@material-ui/core'
 import { format } from 'date-fns'
 
+const formatShowingData = (playid: number) => (data: any) => {
+    const {DateTime, totalseats, ticketType} = data
+    const eventdate = format(DateTime, 'yyyy-MM-dd')
+    const starttime = format(DateTime, 'HH:mm:00')
+    return {playid, eventdate, starttime, totalseats, tickettype: ticketType}
+}
 
 export default function CreateEventPage() {
+    const dispatch = useAppDispatch()
     const [ticketTypes, setTicketTypes] = useState([])
 
     const fetchTicketTypes = async () => {
         const res = await fetch('/api/tickets/type')
-        const tTypes = await res.json()
-        setTicketTypes(tTypes)
+        setTicketTypes(await res.json())
     }
     
     useEffect(() => {
         fetchTicketTypes()
     }, [])
     
+    // TODO: create endpoint that combines /api/create-play & /api/create-showings
     const onSubmit = async (formData: NewEventData) => {
         const {image_url, playname, playdescription, showings} = formData
         
@@ -29,19 +41,21 @@ export default function CreateEventPage() {
         if (createPlayRes.ok) {
             const playData = await createPlayRes.json()
             const { id } = playData.rows[0]
-            const showingdata = showings.map(s => {
-                const {DateTime, totalseats, ticketType} = s
-                const eventdate = format(DateTime, 'yyyy-MM-dd')
-                const starttime = format(DateTime, 'HH:mm:00')
-                const dat = {playid: id, eventdate, starttime, totalseats, tickettype: ticketType}
-                return dat
-            })
-            const createShowingsRes = await fetch('/api/create-showings', {
+            const showingdata = showings.map(formatShowingData(id))
+
+            const postShowings = await fetch('/api/create-showings', {
                 credentials: 'include',
                 headers: { 'Content-Type': 'application/json' },
                 method: 'POST',
                 body: JSON.stringify({showings: showingdata})
             })
+            // update Redux state with new event & available tickets
+            if (postShowings.ok) {
+                console.log('dispatch')
+                dispatch(fetchEventData())
+                dispatch(fetchTicketingData())
+                dispatch(openSnackbar('New Event Created'))
+            }
         }   
         else {
             console.error('New play creation failed', createPlayRes.statusText)
