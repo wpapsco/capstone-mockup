3bf9a57d7adf6812171deffa1d7c6fe379368c67
JECoyle <Zilarrezko76@gmail.com>
2021-08-23 10:53:19 -0700

    Updated database create table queries

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -1,115 +1,164 @@
 -- Create Customers table
 ----Note that this will have to have a prefilled row for anonymous
-create table customers (
-    id serial,
-    custName varchar(255) not null,
-    email varchar(100),
-    phone varchar(15),
-    custAddress varchar(255),
-    newsletter boolean default false,
-    donorBadge varchar(100),
-    seatingAccom boolean,
-    vip boolean default false,
-    primary key(id)
-);
+CREATE TABLE public.customers
+(
+    id integer NOT NULL DEFAULT nextval('customers_id_seq'::regclass),
+    custname character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default",
+    phone character varying(15) COLLATE pg_catalog."default",
+    custaddress character varying(255) COLLATE pg_catalog."default",
+    newsletter boolean DEFAULT false,
+    donorbadge character varying(100) COLLATE pg_catalog."default",
+    seatingaccom boolean,
+    vip boolean DEFAULT false,
+    "volunteer list" boolean NOT NULL DEFAULT false,
+    CONSTRAINT customers_pkey PRIMARY KEY (id)
+)
 
 -- Create Donations table
 create type freq as enum('one-time', 'weekly', 'monthly', 'yearly');
-create table donations (
-    donationId serial,
-    donorId integer,
-    isAnonymous boolean default false,
+CREATE TABLE public.donations
+(
+    donationid integer NOT NULL DEFAULT nextval('donations_donationid_seq'::regclass),
+    donorid integer,
+    isanonymous boolean DEFAULT false,
     amount money,
-    donoName varchar(255),
+    dononame character varying(255) COLLATE pg_catalog."default",
     frequency freq,
-    comments varchar(255),
-    donoDate date,
-    primary key(donationId),
-    foreign key(donorId) references customers(id)
-);
+    comments character varying(255) COLLATE pg_catalog."default",
+    donodate date,
+    CONSTRAINT donations_pkey PRIMARY KEY (donationid),
+    CONSTRAINT donations_donorid_fkey FOREIGN KEY (donorid)
+        REFERENCES public.customers (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
 -- Create Discounts
-create table discounts (
-    id serial,
-    code varchar(255),
+CREATE TABLE public.discounts
+(
+    id integer NOT NULL DEFAULT nextval('discounts_id_seq'::regclass),
+    code character varying(255) COLLATE pg_catalog."default",
     amount money,
-    primary key(id)
-);
+    enddate date,
+    startdate date,
+    usagelimit integer,
+    min_tickets integer,
+    min_events integer,
+    CONSTRAINT discounts_pkey PRIMARY KEY (id)
+)
 
 -- Create Reservation table
-create table reservation (
-    transNo serial,
-    custId integer,
-    eventId integer,
-    eventName varchar(255),
-    eventDate date,
-    showTime time,
-    numTickets integer,
-    primary key(transNo),
-    foreign key(custId) references customers(id)
-);
+CREATE TABLE public.reservation
+(
+    transno integer NOT NULL DEFAULT nextval('reservation_transno_seq'::regclass),
+    custid integer,
+    eventid integer,
+    eventname character varying(255) COLLATE pg_catalog."default",
+    eventdate date,
+    showtime time without time zone,
+    numtickets integer,
+    CONSTRAINT reservation_pkey PRIMARY KEY (transno),
+    CONSTRAINT reservation_custid_fkey FOREIGN KEY (custid)
+        REFERENCES public.customers (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
 -- Create Seasons table
-create table seasons (
-    id serial,
-    name varchar(100),
-    startDate timestamp,
-    endDate timestamp,
-    primary key(id)
-);
+CREATE TABLE public.seasons
+(
+    id integer NOT NULL DEFAULT nextval('seasons_id_seq'::regclass),
+    name character varying(100) COLLATE pg_catalog."default",
+    startdate timestamp without time zone,
+    enddate timestamp without time zone,
+    CONSTRAINT seasons_pkey PRIMARY KEY (id)
+)
 
 -- Create TicketType
-create table ticketType (
-    id integer,
-    name varchar(100),
-    isSeason boolean,
-    seasonId integer,
+CREATE TABLE public.tickettype
+(
+    id integer NOT NULL,
+    name character varying(100) COLLATE pg_catalog."default",
+    isseason boolean,
+    seasonid integer,
     price money,
     concessions money,
-    primary key(id),
-    foreign key(seasonId) references seasons(id)
-);
+    CONSTRAINT tickettype_pkey PRIMARY KEY (id),
+    CONSTRAINT tickettype_seasonid_fkey FOREIGN KEY (seasonid)
+        REFERENCES public.seasons (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
--- Create Plays table
-create table plays (
-    id serial,
-    seasonId integer,
-    playName varchar(255) not null,
-    playDescription varchar(255),
+-- Create Events table
+CREATE TABLE public.events
+(
+    id integer NOT NULL DEFAULT nextval('plays_id_seq'::regclass),
+    seasonid integer,
+    eventname character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    eventdescription character varying(255) COLLATE pg_catalog."default",
     active boolean,
-    primary key(id),
-    foreign key(seasonId) references seasons(id)
-);
+    image_url character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT plays_pkey PRIMARY KEY (id),
+    CONSTRAINT plays_seasonid_fkey FOREIGN KEY (seasonid)
+        REFERENCES public.seasons (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
--- Create Showtime table
-create table showtimes (
-    id serial,
-    playId integer,
-    eventDate date,
-    startTime time,
-    saleStatus boolean,
-    totalSeats integer,
-    availableSeats integer,
-    purchaseURI varchar(255),
-    primary key(id),
-    foreign key(playId) references plays(id)
-);
+-- Create Event_Instances table
+CREATE TABLE public.event_instances
+(
+    id integer NOT NULL DEFAULT nextval('showtimes_id_seq'::regclass),
+    eventid integer,
+    eventdate date,
+    starttime time without time zone,
+    salestatus boolean,
+    totalseats integer,
+    availableseats integer,
+    purchaseuri character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT showtimes_pkey PRIMARY KEY (id),
+    CONSTRAINT showtimes_playid_fkey FOREIGN KEY (eventid)
+        REFERENCES public.events (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
 -- Create Tickets table
-create table tickets (
-    ticketNo serial,
+CREATE TABLE public.tickets
+(
+    ticketno integer NOT NULL DEFAULT nextval('tickets_ticketno_seq'::regclass),
     type integer,
-    eventId integer,
-    custId integer,
+    eventinstanceid integer,
+    custid integer,
     paid boolean,
     active boolean,
-    primary key(ticketNo),
-    foreign key(type) references ticketType(id),
-    foreign key(eventId) references showtimes(id),
-    foreign key(custId) references customers(id)
-);
+    checkedin boolean,
+    checkedin_ts timestamp without time zone,
+    payment_intent character varying COLLATE pg_catalog."default",
+    comments character varying(500) COLLATE pg_catalog."default",
+    CONSTRAINT tickets_pkey PRIMARY KEY (ticketno),
+    CONSTRAINT tickets_custid_fkey FOREIGN KEY (custid)
+        REFERENCES public.customers (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT tickets_eventid_fkey FOREIGN KEY (eventinstanceid)
+        REFERENCES public.event_instances (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT tickets_type_fkey FOREIGN KEY (type)
+        REFERENCES public.tickettype (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
-create table users (
-    id serial primary key,
-    username varchar(255),
-    pass_hash varchar(255)
-);
+CREATE TABLE public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    username character varying(255) COLLATE pg_catalog."default",
+    pass_hash character varying(255) COLLATE pg_catalog."default",
+    is_superadmin boolean DEFAULT false,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_username_key UNIQUE (username)
+)
